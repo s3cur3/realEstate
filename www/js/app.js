6b131d0b748101c72e88e82bc68910349fbfb51d
Tyler Young <s3cur3@gmail.com>
2014-04-09 16:44:52 -0500

    Added support for multiple properties to be created & deleted

--- www/js/app.js
+++ www/js/app.js
@@ -21,101 +21,104 @@ angular.module('RealEstateApp', ['ionic', 'RealEstateApp.controllers', 'RealEsta
         });
     })
 
-    .service('PropertyService', function() {
-        var DEMO = false;
+    .service('CalculatorService', function() {
         var _this = this;
-        var _count = 0;
-        var _Property = function( id ) {
-            if( DEMO ) {
-                return {
-                    id: id,
-                    name: "Sample name " + id,
 
-                    purchasePrice: 63000,
-                    repairsAndImprovements: 500,
-                    appraisedValue: 85000,
+        // Loan
+        this.getLoanAmount = function(property) {
+            return property.purchasePrice;
+        };
 
-                    // Loan
-                    getLoanAmount: function() {
-                        return this.purchasePrice;
-                    },
+        // Income
+        this.getNetAnnualOperatingIncome = function(property) {
+            return property.totalAnnualOperatingIncome - property.totalAnnualOperatingExpenses;
+        };
 
-                    // Income
-                    totalAnnualOperatingIncome: 12186.60,
-                    getNetAnnualOperatingIncome: function() {
-                        return this.totalAnnualOperatingIncome - this.totalAnnualOperatingExpenses;
-                    },
+        // Expenses
+        this.getTotalCashOutlay = function(property) { return 2405.00 };
 
-                    // Expenses
-                    getTotalCashOutlay: function() { return 2405.00 },
-                    totalAnnualOperatingExpenses: 2672.00,
+        // Investment stats
+        this.getCapRate = function(property) {
+            return _this.getNetAnnualOperatingIncome(property) / property.purchasePrice;
+        };
+        this.getCashFlow = function(property) {
+            return 257.03;
+        };
+        this.getDSCR = function(property) {
+            return 1.34;
+        };
+        this.getGRM = function(property) {
+            return 4.95;
+        };
+        this.getLTV = function(property) {
+            return _this.getLoanAmount(property) / property.appraisedValue;
+        };
+    })
 
-                    // Investment stats
-                    getCapRate: function() {
-                        return this.getNetAnnualOperatingIncome() / this.purchasePrice;
-                    },
-                    getCashFlow: function() {
-                        return 257.03;
-                    },
-                    getDSCR: function() {
-                        return 1.34;
-                    },
-                    getGRM: function() {
-                        return 4.95;
-                    },
-                    getLTV: function() {
-                        return this.getLoanAmount() / this.appraisedValue;
-                    }
-                }
-            }
-
+    .service('PropertyService', function() {
+        var _Property = function() {
             return {
-                id: id,
+                id: _this._getNextID(),
                 name: "New property",
 
-                // Loan
-                getLoanAmount: function() {
-                    return this.purchasePrice;
-                },
+                purchasePrice: 63000,
+                repairsAndImprovements: 500,
+                appraisedValue: 85000,
 
                 // Income
                 totalAnnualOperatingIncome: 12186.60,
-                getNetAnnualOperatingIncome: function() {
-                    return this.totalAnnualOperatingIncome - this.totalAnnualOperatingExpenses;
-                },
 
                 // Expenses
-                getTotalCashOutlay: function() { return 2405.00 },
-                totalAnnualOperatingExpenses: 2672.00,
+                totalAnnualOperatingExpenses: 2672.00
 
                 // Investment stats
-                getCapRate: function() {
-                    return this.getNetAnnualOperatingIncome() / this.purchasePrice;
-                },
-                getCashFlow: function() {
-                    return 257.03;
-                },
-                getDSCR: function() {
-                    return 1.34;
-                },
-                getGRM: function() {
-                    return 4.95;
-                },
-                getLTV: function() {
-                    return this.getLoanAmount() / this.appraisedValue;
-                }
             }
         };
 
+        this._getNextID = function() {
+            var all = _this.allProperties();
+
+            var id;
+            if( all.length > 0 ) {
+                id = all[ all.length - 1 ].id + 1;
+            console.log("Found ID to be " + id);
+            } else {
+                id = 0;
+                console.log("No known properties!");
+                console.log(all);
+            }
+
+            return id;
+        };
+
         this.newProperty = function() {
-            var prop = _Property(_count++);
+            var prop = _Property();
+            console.log("Created new property with ID " + prop.id);
             _this.save(prop);
 
             _this.setLastActiveIndex(prop.id);
             return  prop;
         };
 
+        this.getPropertyByID = function(id) {
+            var properties = _this.allProperties();
+
+
+            if( properties.length > 0 ) {
+                for( var i = 0; i < properties.length; i++ ) {
+                    if( properties[i].id == id ) {
+                        return properties[i];
+                    }
+                }
+
+                console.error("ERROR: Couldn't find property " + id);
+                return properties[0];
+            }
+            return _this.newProperty();
+        };
+
         this.allProperties = function() {
+            // TODO: This is RIDICULOUSLY inefficient --- it's called all the damn time!
             if( DEMO ) {
                 var list = [];
                 for( var i = 0; i < 10; i++ ) {
@@ -131,6 +134,15 @@ angular.module('RealEstateApp', ['ionic', 'RealEstateApp.controllers', 'RealEsta
                 if( !Array.isArray(properties) ) {
                     return [];
                 }
+
+                // TODO: Remove this?
+                // Nuke any properties without an id
+                _.remove( properties, function(prop) {
+                    if( typeof prop == "object" && prop ) {
+                        return prop.id == null;
+                    }
+                    return true; // Not an object; what's it doing here??
+                });
 
                 return properties;
             }
@@ -191,7 +203,7 @@ angular.module('RealEstateApp', ['ionic', 'RealEstateApp.controllers', 'RealEsta
 
         this.delete = function( property ) {
             var properties = _this.allProperties();
-            properties = _.remove( properties, function(prop) {
+            _.remove( properties, function(prop) {
                 return prop.id == property.id;
             });
             _this.save(properties);
@@ -201,9 +213,29 @@ angular.module('RealEstateApp', ['ionic', 'RealEstateApp.controllers', 'RealEsta
             return parseInt(window.localStorage['lastActiveProperty']) || 0;
         };
 
-        this.setLastActiveIndex = function( index ) {
-            window.localStorage['lastActiveProperty'] = index;
+        this.setLastActiveIndex = function( indexOrProperty ) {
+            if( typeof indexOrProperty == "number" ) {
+                window.localStorage['lastActiveProperty'] = indexOrProperty;
+            } else {
+                window.localStorage['lastActiveProperty'] = _this._getIndexOfProperty(indexOrProperty);
+            }
+
         };
+
+        this._getIndexOfProperty = function( prop ) {
+            assert(typeof prop == "object", "Sought property is not a valid property object.");
+
+            var all = _this.allProperties();
+            for( var i = 0; i < all.length; i++ ) {
+                if( all[i].id == prop.id ) {
+                    return i;
+                }
+            }
+            return -1;
+        };
+
+        var DEMO = false;
+        var _this = this;
     })
 
     // Define our routes
@@ -227,16 +259,6 @@ angular.module('RealEstateApp', ['ionic', 'RealEstateApp.controllers', 'RealEsta
                 }
             })
 
-            .state('app.analysis', {
-                url: "/property/analysis",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/analysis.html",
-                        controller: 'AnalysisCtrl'
-                    }
-                }
-            })
-
             .state('app.properties', {
                 url: "/properties",
                 views: {
@@ -247,16 +269,6 @@ angular.module('RealEstateApp', ['ionic', 'RealEstateApp.controllers', 'RealEsta
                 }
             })
 
-            /*.state('app.property', {
-             url: "/property",
-             views: {
-             'menuContent': {
-             templateUrl: "templates/property.html",
-             controller: 'PropertyCtrl'
-             }
-             }
-             })*/
-
             .state('app.property', {
                 url: "/properties/:propertyId",
                 views: {
@@ -265,7 +277,19 @@ angular.module('RealEstateApp', ['ionic', 'RealEstateApp.controllers', 'RealEsta
                         controller: 'PropertyCtrl'
                     }
                 }
-            });
+            })
+
+            .state('app.analysis', {
+                url: "/property/:propertyId/analysis",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/analysis.html",
+                        controller: 'AnalysisCtrl'
+                    }
+                }
+            })
+
+        ;
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/properties');
     });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,11 @@
 angular.module('RealEstateApp.controllers', [])
 
-    .controller('AppCtrl', ['$scope', 'PropertyService', function( $scope, PropertyService ) {
+    .controller('AppCtrl', ['$scope', '$location', '$ionicModal', 'PropertyService', 'CalculatorService', function( $scope, $location, $ionicModal, PropertyService, CalculatorService ) {
         // Set up property functionality
         $scope.properties = PropertyService.allProperties();
-        $scope.property = PropertyService.newProperty();
+        $scope.property = $scope.properties[ PropertyService.getLastActiveIndex() ];
+
+        $scope.calc = CalculatorService;
 
         $scope.criteria = {
             capRate: 12,
@@ -15,19 +17,19 @@ angular.module('RealEstateApp.controllers', [])
 
         $scope.analysis = {
             capRateGood: function() {
-                return $scope.property.getCapRate() * 100 >= $scope.criteria.capRate;
+                return $scope.calc.getCapRate( $scope.property ) * 100 >= $scope.criteria.capRate;
             },
             cashFlowGood: function() {
-                return $scope.property.getCashFlow() * 100 >= $scope.criteria.cashFlow;
+                return $scope.calc.getCashFlow( $scope.property ) * 100 >= $scope.criteria.cashFlow;
             },
             dscrGood: function() {
-                return $scope.property.getDSCR() > $scope.criteria.dscr;
+                return $scope.calc.getDSCR( $scope.property ) > $scope.criteria.dscr;
             },
             grmGood: function() {
-                return $scope.property.getGRM() < $scope.criteria.grm;
+                return $scope.calc.getGRM( $scope.property ) < $scope.criteria.grm;
             },
             ltvGood: function() {
-                return $scope.property.getLTV() * 100 < $scope.criteria.ltv;
+                return $scope.calc.getLTV( $scope.property ) * 100 < $scope.criteria.ltv;
             }
         };
 
@@ -38,6 +40,28 @@ angular.module('RealEstateApp.controllers', [])
             }
             return giveGreenLight;
         };
+
+
+        // Functionality for the Delete Modal
+        $ionicModal.fromTemplateUrl('delete.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal){
+            $scope.deleteModal = modal;
+        });
+
+        $scope.delete = function(p) {
+            $scope.propertyToDelete = p;
+            $scope.deleteModal.show();
+        };
+        $scope._delete = function() {
+            $scope.deleteModal.hide();
+            PropertyService.delete($scope.propertyToDelete);
+            $location.path('/properties');
+        };
+        $scope.closeDelete = function() {
+            $scope.deleteModal.hide();
+        }
     }])
 
 
@@ -45,32 +69,47 @@ angular.module('RealEstateApp.controllers', [])
 
     })
 
-    .controller('PropertiesCtrl', ['$scope', 'PropertyService', function($scope, PropertyService) {
+    .controller('PropertiesCtrl', ['$scope', '$location', 'PropertyService', function($scope, $location, PropertyService) {
+        $scope.properties = PropertyService.allProperties();
+        if( $scope.properties.length == 0 ) {
+            $scope.property = PropertyService.newProperty();
+            $scope.properties = PropertyService.allProperties();
+        }
+
+
         $scope.selectProperty = function(project, index) {
-            $scope.activeProperty = project;
+            $scope.property = project;
 
-            if( index == null ) {
-                index = $scope.activeProperty.id;
-            }
-
-            PropertyService.setLastActiveIndex(index);
-            $scope.sideMenuController.close();
+            PropertyService.setLastActiveIndex(index || project);
         };
 
         $scope.addProperty = function() {
+            console.log("Adding property");
             var p = PropertyService.newProperty();
             $scope.properties = PropertyService.allProperties();
-            $scope.selectProperty(PropertyService.newProperty());
+            $scope.selectProperty(p);
 
             console.log("Setting path to " + '/properties/' + PropertyService.getLastActiveIndex());
-            $location.path('/properties/' + PropertyService.getLastActiveIndex());
+            $location.path('/#/properties/' + PropertyService.getLastActiveIndex());
         }
 
     }])
 
-    .controller('PropertyCtrl', function($scope) {
+    .controller('PropertyCtrl', ['$scope', '$location', 'PropertyService', function($scope, $location, PropertyService) {
+        // Update the currently-in-use property when we load this one
+        var re = /[0-9]+$/;
+        var id = $location.url().match(re);
+        $scope.property = PropertyService.getPropertyByID( id );
 
-    })
+        // Update the master property list whenever we modify this property
+        $scope.$watchCollection('property', function(updatedProperty, oldProperty) {
+            if( typeof updatedProperty == "object" && updatedProperty ) {
+                PropertyService.save(updatedProperty);
+                $scope.properties = PropertyService.allProperties();
+            }
+        });
+
+    }])
 
     .controller('AnalysisCtrl', function($scope) {
 
